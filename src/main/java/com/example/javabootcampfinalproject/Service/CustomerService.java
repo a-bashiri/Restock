1edd9dec039fb9e86d5e691f4c3ab28e909a637b
a-bashiri <abdulazizbashiri@gmail.com>
2023-02-26 18:36:31 +0300

    test

--- src/main/java/com/example/javabootcampfinalproject/Service/CustomerService.java
+++ src/main/java/com/example/javabootcampfinalproject/Service/CustomerService.java
@@ -28,5 +28,6 @@ public class CustomerService {
         oldCustomer.setName(dto.getName());
         oldCustomer.setLocation(dto.getLocation());
         customerRepository.save(oldCustomer);
+        // hello
     }
 }
