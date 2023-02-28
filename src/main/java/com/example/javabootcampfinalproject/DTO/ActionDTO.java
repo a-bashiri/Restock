167f4476f11608715b1a13f00e78b8b093a30bae
Yousef <yousef1415@hotmail.com>
2023-02-28 19:41:07 +0300

    Numbering actions
    Signed-off-by: Yousef <yousef1415@hotmail.com>

--- src/main/java/com/example/javabootcampfinalproject/DTO/ActionDTO.java
+++ src/main/java/com/example/javabootcampfinalproject/DTO/ActionDTO.java
@@ -1,6 +1,8 @@
 package com.example.javabootcampfinalproject.DTO;
 
 import com.example.javabootcampfinalproject.Utility.Enum.Action;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,5 +11,6 @@ import lombok.Data;
 @AllArgsConstructor
 public class ActionDTO {
     @NotNull
+    @Enumerated(EnumType.ORDINAL)
     private Action action;
 }
