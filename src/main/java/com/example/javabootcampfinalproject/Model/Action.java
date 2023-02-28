767005c527f9b6f78cc7554d3a39df8c8208c0ef
Yousef <yousef1415@hotmail.com>
2023-02-28 17:04:03 +0300

    Update Action
    Signed-off-by: Yousef <yousef1415@hotmail.com>

--- src/main/java/com/example/javabootcampfinalproject/Model/Action.java
+++ src/main/java/com/example/javabootcampfinalproject/Model/Action.java
@@ -3,5 +3,10 @@ package com.example.javabootcampfinalproject.Model;
 public enum Action {
     ACCEPT,
     REJECT,
-    FULFILL
+    FULFILL,
+    CANCEL,
+    PICKED_FOR_DELIVERY,
+    PICKED_FOR_RETURNING,
+    DELIVERED,
+    RETURNED
 }
